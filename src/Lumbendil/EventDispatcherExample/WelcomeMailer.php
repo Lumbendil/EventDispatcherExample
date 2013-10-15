fbd4a00897c9ebd84c4aa0224b1bfde5ed15a550
Roger Llopart Pla <lumbendil@gmail.com>
2013-10-16 00:24:46 +0200

    Added event dispatcher.

--- src/Lumbendil/EventDispatcherExample/WelcomeMailer.php
+++ src/Lumbendil/EventDispatcherExample/WelcomeMailer.php
@@ -2,16 +2,37 @@
 
 namespace Lumbendil\EventDispatcherExample;
 
+use Symfony\Component\EventDispatcher\EventDispatcherInterface;
+use Symfony\Component\EventDispatcher\GenericEvent;
+
 class WelcomeMailer
 {
+    private $eventDispatcher;
+
+    public function __construct(EventDispatcherInterface $eventDispatcher)
+    {
+        $this->eventDispatcher = $eventDispatcher;
+    }
+
     /**
      * @param string[] $emails An array of emails.
      */
     public function sendEmails(array $emails)
     {
+        $this->eventDispatcher->dispatch('welcome_mailer.mailer_start');
+
         foreach ($emails as $email) {
-            $this->sendEmail($email);
+            $event = new GenericEvent();
+            $event['email'] = $email;
+
+            if ($this->sendEmail($email)) {
+                $this->eventDispatcher->dispatch('welcome_mailer.send_successful', $event);
+            } else {
+                $this->eventDispatcher->dispatch('welcome_mailer.send_failed', $event);
+            }
         }
+
+        $this->eventDispatcher->dispatch('welcome_mailer.mailer_end');
     }
 
     /**
@@ -22,6 +43,14 @@ class WelcomeMailer
     private function sendEmail($email)
     {
         $message = "Welcome $email";
+
+        $event = new GenericEvent();
+        $event['email'] = $email;
+        $event['message'] = $message;
+
+        $this->eventDispatcher->dispatch('welcome_mailer.mail_prepared', $event);
+        $message = $event['message'];
+
         echo "Sending message to $email: $message", PHP_EOL;
         return true;
     }
--- src/main.php
+++ src/main.php
@@ -1,11 +1,15 @@
 <?php
 use Lumbendil\EventDispatcherExample\WelcomeMailer;
+use Symfony\Component\EventDispatcher\EventDispatcher;
 
 require_once __DIR__ . "/../vendor/autoload.php";
 
-$mailer = new WelcomeMailer();
+$dispatcher = new EventDispatcher();
+$mailer = new WelcomeMailer($dispatcher);
+
 $emails = array(
     'bob@example.com',
     'alice@example.com',
 );
+
 $mailer->sendEmails($emails);
